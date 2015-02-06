bd01aa1b0ebe968a1f89684528f8bb2d589be370
Jonathan Damico <jj_damico@yahoo.com>
2015-02-05 19:51:39 -0800

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -8,16 +8,28 @@
 {
     character = (Character*)[CCBReader load:@"Character"];
     [physicsNode addChild:character];
+    [self addObstacle];
+    timeSinceObstacle = 0.0f;
 }
 
 -(void)update:(CCTime)delta
 {
-    // put update code here
+    //Increment the time since the last obsticle
+    timeSinceObstacle += delta;
+    
+    //See if two seconds have passed
+    if(timeSinceObstacle>2.0f) {
+        
+        //Add an obstacle
+        [self addObstacle];
+        
+        //Reset Timer
+        timeSinceObstacle = 0.0f;
+    }
 }
 
 -(void)touchBegan:(CCTouch *)touch withEvent:(CCTouchEvent *)event {
     [character flap];
-    [self addObstacle];
 }
 
 @end
